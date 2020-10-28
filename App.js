2c8cb32f34bcde26032c5bb3a03b33560d4d7b1a
Your Name <fitzgeraldkachi@gmail.com>
2020-10-28 20:58:15 +0100

    react-navigationsV5

--- App.js
+++ App.js
@@ -1,21 +1,82 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import "react-native-gesture-handler"
+import React from 'react'
+import { StyleSheet, Text, View, Button } from 'react-native'
+import { NavigationContainer } from "@react-navigation/native"
+import {createDrawerNavigator} from "@react-navigation/drawer"
+import RoutesTab from "./navigation/RoutesTab"
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import {DrawerContent} from "./navigation/DrawerContent"
+import SupportScreen from "./screens/SupportScreen"
+import SettingsScreen from "./screens/SettingsScreen"
+import BookmarkScreen from "./screens/BookmarkScreen"
+import ProfileScreen from "./screens/ProfileScreen"
+
+// import {HomeStackScreen,DetailStackScreen } from "./navigation/RoutesTab"
+
+
+const Drawer = createDrawerNavigator() 
+
+
+
+// const position = pos == "right"?"left":"right";
+
+
+const App = () => {
+    return (
+        <NavigationContainer >
+            <Drawer.Navigator drawerContent={props => <DrawerContent {...props} />}  >
+                <Drawer.Screen name="HomeDrawer" component={RoutesTab}  />
+                <Drawer.Screen name="SupportScreen" component={SupportScreen}  />
+                <Drawer.Screen name="SettingsScreen" component={SettingsScreen} /> 
+                <Drawer.Screen name="BookmarkScreen" component={BookmarkScreen} /> 
+                <Drawer.Screen name="ProfileScreen" component={ProfileScreen} /> 
+            </Drawer.Navigator>
+        </NavigationContainer>
+    )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
+
+const styles = StyleSheet.create({})
+
+
+
+    // <Drawer.Navigator initialRouteName="Home">
+    //     <Drawer.Screen name="Home" component={HomeScreen}/>
+    //     <Drawer.Screen name="Detail" component={DetailScreen} />
+    // </Drawer.Navigator> 
+
+
+                    //////////////////////////////////////////////
+                
+                    
+
+                    // const Stack = createStackNavigator()
+                    // const Drawer = createDrawerNavigator() 
+                    
+                    
+                    //     const createHomeStack = ({navigation}) => 
+                    //     <Stack.Navigator screenOptions={{
+                    //         headerStyle:{backgroundColor:"#009387"}, headerTintColor:"#fff", headerTitleStyle:{fontWeight:"bold"} 
+                    //     }}>
+                    //         <Stack.Screen name="Home" component={HomeScreen} 
+                    //         options={{ title:"HomeScreen",
+                    //          headerLeft:()=> ( <Button title="Menu" onPress={()=> navigation.openDrawer() }/>)
+                    //          }} />
+                    //         <Stack.Screen name="Detail" component={DetailScreen} />
+                    //     </Stack.Navigator>
+                    
+                    // const App = () => {
+                    //     return (
+                    //         <NavigationContainer >
+                    //             <Drawer.Navigator screenOptions={{
+                    //                 headerStyle:{backgroundColor:"#009387"}, headerTintColor:"#fff", headerTitleStyle:{fontWeight:"bold"} 
+                    //             }}>
+                    //                 <Drawer.Screen name="Home" component={createHomeStack} 
+                    //                 options={{ title:"OverView" }} />
+                    //                 <Drawer.Screen name="Detail" component={DetailScreen} />
+                    //             </Drawer.Navigator>
+                    //         </NavigationContainer>
+                    //     )
+                    // }
+
