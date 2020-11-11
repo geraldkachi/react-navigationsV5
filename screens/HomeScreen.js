f5bae40699b3e3de2bb4fa204cddf5bf3901d7ee
Your Name <fitzgeraldkachi@gmail.com>
2020-11-11 02:59:42 +0100

    react native validation and authentication

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,17 @@
 import React from 'react'
-import { StyleSheet, Text, View, Button } from 'react-native'
+import { StyleSheet, Text, View, Button, StatusBar } from 'react-native'
+import {useTheme} from "@react-navigation/native"
 
 const HomeScreen = ({navigation}) => {
+
+    const {colors} = useTheme()
+
+    const theme = useTheme() 
+
     return (
         <View style={styles.container}>
-            <Text>HomeScreen</Text>
+            <StatusBar barStyle={theme.dark ? "light-content" : "dark-content"} />
+            <Text style={{color: colors.text}}>HomeScreen</Text>
             <Button title="Go to DetailScreen" onPress={()=> navigation.navigate("Detail")} />
         </View>
     )
